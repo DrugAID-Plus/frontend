c0bf3b5e7e2b86ab62ab0a5ac30bedf31ac98483
Brandon Fan <brandonfan1256@gmail.com>
2019-02-16 12:50:26 -0500

    add initial search service

---
+++ src/app/core/services/search.service.ts
@@ -0,0 +1,55 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+/**
+ * Search service to handle all requests to elastic search and searching the Bible
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  /**
+   * List of search results
+   */
+  results: any[] = [];
+  /**
+   * String to see if data has been found
+   */
+  found = 'not_found';
+  /**
+   * Elasticsearch URL provided by the environment
+   */
+  searchUrl: string;
+  /**
+   * Initializes dependencies and does dependency injection
+   * @param http Http Client dependency to handle http requests
+   */
+  constructor(private http: HttpClient) {
+    this.searchUrl = environment.es_url;
+  }
+  /**
+   * Searches ES instance given a query and a sort type
+   * @param query Search string
+   * @param sort_type Sort type to use
+   */
+  searchES(query, sort_type = 'relevant') {
+    this.found = 'pending';
+    this.http.get(`${ this.searchUrl }/search?term=${ query }&sort_type=${ sort_type }`).subscribe(res => {
+      if (res[ 'results' ].length === 0) {
+        this.found = 'not_found';
+        this.results = [];
+      } else {
+        this.found = 'found';
+        for (let i = 0; i < res[ 'results' ].length; i++) {
+          res[ 'results' ][ i ][ 'search_index' ] = i;
+        }
+        this.results = res[ 'results' ];
+      }
+    });
+  }
+  getDrugByID(index) {
+    return this.results[ index ];
+  }
+}
