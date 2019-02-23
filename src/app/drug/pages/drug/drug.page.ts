b71fe476ace7e999adf8782600f74513a8ecb2e1
Brandon Fan <brandonfan1256@gmail.com>
2019-02-23 13:19:47 -0500

    feat(drug): add initial drug page css and ts

--- src/app/drug/pages/drug/drug.page.ts
+++ src/app/drug/pages/drug/drug.page.ts
@@ -9,11 +9,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: [ './drug.page.scss' ]
 })
 export class DrugComponent implements OnInit {
-  drug: any;
+  drug: any = undefined;
+  reported_effects_keys: any = undefined;
+  reported_effects: any = undefined;
+  tab: 'side-effects' | 'general-information' = 'side-effects';
   id;
-  constructor(private _route: ActivatedRoute, private _search: SearchService) { }
-
-  ngOnInit() {
+  constructor(private _route: ActivatedRoute, private _search: SearchService) {
     this._route.params.subscribe(params => {
       if (!isNaN(params[ 'id' ])) {
         this.id = +params[ 'id' ];
@@ -22,10 +23,29 @@ export class DrugComponent implements OnInit {
         this.id = params[ 'id' ];
         this._search.getDrugFromDatabase(this.id).subscribe((res) => {
           this.drug = res[ 'result' ][ 0 ];
-          console.log(this.drug);
+          this.reported_effects_keys = Object.keys(this.drug[ 'side_effects' ][ 'reported_effects' ]);
+          this.reported_effects = this.drug[ 'side_effects' ][ 'reported_effects' ];
+          console.log(this.reported_effects);
         });
       }
     });
+  }
+
+  ngOnInit() {
+
+  }
+
+  switchTab(tab) {
+    this.tab = tab;
+  }
+
+  capitalize(word) {
+    const split_word = word.split('-');
+    const final = [];
+    split_word.forEach(word_val => {
+      final.push(word_val.substring(0, 1).toUpperCase() + word_val.substring(1));
+    });
+    return final.join(' ');
   }
 
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: [ './navbar.component.scss' ]
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  isDrugPage = false;
+  constructor(private _router: Router, private _location: Location) { }
 
   ngOnInit() {
+    this._router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isDrugPage = this.checkDrugUrl(event.url);
+      });
+  }
+
+  checkDrugUrl(url) {
+    return url.includes('/drugs/');
+  }
+
+  goBack() {
+    this._location.back();
   }
 
 }
