1a91696592a3d83f43f5f94103788da7cef0cc56
Brandon Fan <brandonfan1256@gmail.com>
2020-02-25 16:50:40 -0500

    update to angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: 'app/home/home.module#HomeModule' },
-  { path: 'search', loadChildren: 'app/search/search.module#SearchModule' },
-  { path: 'drugs', loadChildren: 'app/drug/drug.module#DrugModule' },
+  { path: '', loadChildren: () => import('app/home/home.module').then(m => m.HomeModule) },
+  { path: 'search', loadChildren: () => import('app/search/search.module').then(m => m.SearchModule) },
+  { path: 'drugs', loadChildren: () => import('app/drug/drug.module').then(m => m.DrugModule) },
   {
     path: '**', redirectTo: '', pathMatch: 'full'
   }
